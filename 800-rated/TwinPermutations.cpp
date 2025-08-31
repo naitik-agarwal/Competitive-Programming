04aaeb8083ce1654be271db8411171e998f25b9f
naitik-agarwal <a.naitik@iitg.ac.in>
2025-08-31 11:22:20 +0530

    TLE-800

---
+++ 800-rated/TwinPermutations.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#pragma GCC optimize("Ofast,unroll-loops")
+
+#define int long long
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int num=n+1;
+        for(int i=0;i<n;i++){
+            cout<<num-a[i]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
